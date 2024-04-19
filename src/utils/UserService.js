90ccc660328bd0184cd7b7e3efdafc4123c5b871
2170chm <2170chm@gmail.com>
2024-04-19 01:45:29 -0700

    Modified User service util to adjust request structure

--- src/utils/UserService.js
+++ src/utils/UserService.js
@@ -9,19 +9,21 @@ class UserService {
     );
   }
 
-  async getUsernameById(userId) {
+  async getUserById(userId) {
     const query = gql`
       query getUserById($id: ID!) {
         getUserById(id: $id) {
           username
+          privilege
         }
       }
     `;
     const variables = { id: userId };
+    console.log("getUserById query variables: ", variables);
     try {
       const data = await this.graphqlClient.request(query, variables);
       if (data.getUserById) {
-        return data.getUserById.username;
+        return data.getUserById;
       } else {
         throw new Error("User not found");
       }
